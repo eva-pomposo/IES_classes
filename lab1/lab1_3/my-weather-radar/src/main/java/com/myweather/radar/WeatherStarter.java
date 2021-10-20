9a118d69d1c049d472534e4596f1c64e310845b3
eva-pomposo <evabartolomeu@ua.pt>
2021-10-20 02:18:08 +0100

    update .gitignore & begin the exercice 1.3

---
+++ lab1/lab1_3/my-weather-radar/src/main/java/com/myweather/radar/WeatherStarter.java
@@ -0,0 +1,54 @@
+package main.java.com.myweather.radar;
+
+import retrofit2.Call;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+import main.java.com.myweather.radar.IpmaCityForecast;
+import main.java.com.myweather.radar.IpmaService;
+
+import java.util.logging.Logger;
+
+/**
+ * demonstrates the use of the IPMA API for weather forecast
+ */
+public class WeatherStarter {
+
+    //private static final int CITY_ID_AVEIRO = 1010500;
+    /*
+    loggers provide a better alternative to System.out.println
+    https://rules.sonarsource.com/java/tag/bad-practice/RSPEC-106
+     */
+    private static final Logger logger = Logger.getLogger(WeatherStarter.class.getName());
+
+    public static void  main(String[] args ) {
+        final int CITY_ID = Integer.parseInt(args[0]);
+
+        /*
+        get a retrofit instance, loaded with the GSon lib to convert JSON into objects
+         */
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("http://api.ipma.pt/open-data/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        IpmaService service = retrofit.create(IpmaService.class);
+        //Call<IpmaCityForecast> callSync = service.getForecastForACity(CITY_ID_AVEIRO);
+        Call<IpmaCityForecast> callSync = service.getForecastForACity(CITY_ID);
+
+        try {
+            Response<IpmaCityForecast> apiResponse = callSync.execute();
+            IpmaCityForecast forecast = apiResponse.body();
+
+            if (forecast != null) {
+                logger.info( "max temp for today: " + forecast.getData().
+                        listIterator().next().getTMax());
+            } else {
+                logger.info( "No results!");
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+
+    }
+}
