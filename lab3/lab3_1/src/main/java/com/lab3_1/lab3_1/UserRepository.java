81db92c6fba556e1b71e5b5f84cd03d69a74233e
eva-pomposo <evabartolomeu@ua.pt>
2021-11-11 10:34:49 +0000

    3.1a e 3.1c done

---
+++ lab3/lab3_1/src/main/java/com/lab3_1/lab3_1/UserRepository.java
@@ -0,0 +1,13 @@
+package com.lab3_1.lab3_1;
+
+import com.lab3_1.lab3_1.User;
+import java.util.List;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Long> {
+    
+    List<User> findByName(String name);
+    
+}
